4813e5e0c8a18b399c5ff63281c8f62ff1a759cb
Owen Delisle <owen37164@gmail.com>
2020-05-06 17:49:42 -0700

    added script for linting source files in FlowCommonWeb

---
+++ ESLintFlowCommon.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+fix = ARGV.include? '--fix'
+
+Dir.each_child("Sources") {|filename|
+    #excluding minified files
+    if !filename.include? ".min"
+        puts "ESLinting #{filename}"
+        if fix
+            abort unless system "cd Sources && npx eslint #{filename} --fix"
+            else
+            abort unless system "cd Sources && npx eslint #{filename}"
+        end
+    end
+}
+
+puts "Passed!"
