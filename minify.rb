0a9fcd2f499a42a5714c9cf044fc6bcd66b23327
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2020-05-19 15:05:23 -0700

    update minify.rb to create file with name: flowcommon.min.js (#7)

--- minify.rb
+++ minify.rb
@@ -4,8 +4,9 @@ puts "Minifying FlowCommonWeb files."
 flowCommonWebDir = "#{__dir__}/Sources/FlowCommonWebFiles"
 flowCommonWebFiles = "Track.js Animation.js player.js web-animations.min.js ToggleButton.js"
 outputDir = "#{__dir__}"
+filename = "flowcommon.min.js"
 
-abort unless system "cd #{flowCommonWebDir} && uglifyjs #{flowCommonWebFiles} --output #{outputDir}/flowCommonWeb.min.js"
+abort unless system "cd #{flowCommonWebDir} && uglifyjs #{flowCommonWebFiles} --output #{outputDir}/#{filename}"
 
 puts "Successfully Minified FlowCommonWeb files!"
 
