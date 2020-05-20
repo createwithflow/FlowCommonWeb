f4fcf872e821a9ebe52c8b04aff91c20ea380346
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2020-05-20 15:23:59 -0700

    generated 3 different version for FlowCommon Web for the CDN

--- minify.rb
+++ minify.rb
@@ -1,11 +1,30 @@
 #!/usr/bin/env ruby
 
 puts "Minifying FlowCommonWeb files."
-flowCommonWebDir = "#{__dir__}/Sources/FlowCommonWebFiles"
-flowCommonWebFiles = "Track.js Animation.js player.js web-animations.min.js ToggleButton.js"
-outputDir = "#{__dir__}"
-filename = "flowcommon.min.js"
 
-abort unless system "cd #{flowCommonWebDir} && uglifyjs #{flowCommonWebFiles} --compress --output #{outputDir}/#{filename}"
+flowCommonWebDir = "#{__dir__}/Sources/FlowCommonWebFiles"
+waapi = "web-animations.min.js"
+flowCommonWebFiles = [
+    "Track.js",
+    "Animation.js",
+    "player.js",
+    "ToggleButton.js"
+]
+
+outputDir = "#{__dir__}/generated"
+system "mkdir #{outputDir}"
+
+flowcommon = "flowcommon.js"
+flowcommonMin = "flowcommon.min.js"
+flowcommonWaapiMin = "flowcommon-waapi.min.js"
+
+puts "Creating flowcommon.js"
+abort unless system "cd #{flowCommonWebDir} && uglifyjs #{flowCommonWebFiles.join(' ')} --beautify --output #{outputDir}/#{flowcommon}"
+
+puts "Creating flowcommon.min.js"
+abort unless system "cd #{flowCommonWebDir} && uglifyjs #{flowCommonWebFiles.join(' ')} --compress --output #{outputDir}/#{flowcommonMin}"
+
+puts "Creating flowcommon-waapi.min.js"
+abort unless system "cd #{flowCommonWebDir} && uglifyjs #{flowCommonWebFiles.join(' ')} #{waapi} --compress --output #{outputDir}/#{flowcommonWaapiMin}"
 
 puts "Successfully Minified FlowCommonWeb files!"
